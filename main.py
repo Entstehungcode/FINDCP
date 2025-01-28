c4b55346a37f7740061862f3653efb5b073a4fa2
Entstehungcode <100434432+Entstehungcode@users.noreply.github.com>
2025-01-28 10:21:13 +0000

    All runnning good

--- main.py
+++ main.py
@@ -6,19 +6,17 @@ postal_code_manager = PostalCodeManager()
 def main():
     st.title("Consulta de Códigos Postales")
 
-    postal_code = st.text_input("Introduce el Código Postal", help="El CP deb tener 5 dígitos")
+    postal_code = st.text_input("Introduce el Código Postal", help="El CP debe tener 5 dígitos")
 
     if postal_code:
-        try:
-            postal_code = int(postal_code)
-            routes = postal_code_manager.get_routes_for_postal_code(postal_code)
-            if routes:
-                st.success(f"Código Postal {postal_code} asociado a: ")
-                st.info(f" Jaula: {','.join(map(str, routes))}")
-            else:
-                st.error(f"No se encontró información para el código postal {postal_code}")
-        except ValueError:
-            st.warning("Por favor, ingresa un código postal válido (5 dígitos)")
+        # No convertimos postal_code a int porque las claves del diccionario son cadenas
+        postal_code = postal_code.strip()  # Eliminamos espacios en blanco
+        routes = postal_code_manager.get_routes_for_postal_code(postal_code)
+        if routes:
+            st.success(f"Código Postal {postal_code} asociado a: ")
+            st.info(f"Jaula: {', '.join(map(str, routes))}")
+        else:
+            st.error(f"No se encontró información para el código postal {postal_code}")
 
 if __name__ == "__main__":
     main()
