24af1ae878116e08100ce48de39c2ba8f0b258af
Entstehungcode <100434432+Entstehungcode@users.noreply.github.com>
2025-02-11 13:19:25 +0000

    En esta versión se muestra los nombres de los conductores por ruta

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from postalcodes import PostalCodeManager
+from postalcodes import PostalCodeManager  # Asegúrate de que este archivo contenga la clase actualizada
 
 postal_code_manager = PostalCodeManager()
 
@@ -9,14 +9,16 @@ def main():
     postal_code = st.text_input("Introduce el Código Postal", help="El CP debe tener 5 dígitos")
 
     if postal_code:
-        # No convertimos postal_code a int porque las claves del diccionario son cadenas
         postal_code = postal_code.strip()  # Eliminamos espacios en blanco
-        routes = postal_code_manager.get_routes_for_postal_code(postal_code)
-        if routes:
-            st.success(f"Código Postal {postal_code} asociado a: ")
-            st.info(f"Jaula: {', '.join(map(str, routes))}")
+        results = postal_code_manager.get_info_for_postal_code(postal_code)
+
+        if results:
+            st.success(f"Información para el Código Postal {postal_code}:")
+            for entry in results:
+                st.info(f"**Jaula:** {entry['jaula']}  \n**Nombre:** {entry['nombre']}")
         else:
             st.error(f"No se encontró información para el código postal {postal_code}")
 
 if __name__ == "__main__":
     main()
+
